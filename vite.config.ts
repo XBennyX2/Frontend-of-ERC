16f30183a2f2506fe01ded52f574672dbf9361f6
XBennyX2 <ezeraben47@gmail.com>
2025-06-07 20:36:33 +0300

    Making it ready for the backend api calls

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy:{
+      '/api':{
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
